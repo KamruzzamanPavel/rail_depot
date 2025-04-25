7f0f44550e613f3d5533be855df96c73a4c36170
KamruzzamanPavel <kamruzzamanpavel88@gmail.com>
2025-04-25 16:29:51 +0600

    login before crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_product, only: %i[ show edit update destroy ]
 
   # GET /products or /products.json
